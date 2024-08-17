54ae75f81038380852db87ade43e32bec515f7c9
Matheus Iago dos Santos Silva <162377902+iago-developer@users.noreply.github.com>
2024-08-17 16:31:44 -0300

    Atualização dos componentes React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./componentes/images/git-logo.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+ return (
+  <>
+   <header>
+    <div className="header-title">
+      <h1>Git Learning...</h1>
     </div>
-  );
+    <div className="header-nav">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Project</li>
+        </ul>
+      </nav>
+    </div>
+    <div className="header-buttonH">
+      <input type="checkbox" name="buttonH" id="buttonH" />
+      <label>
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+    </div>
+   </header>
+  </>
+ )
 }
-
-export default App;
