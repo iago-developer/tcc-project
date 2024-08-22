5b60ef29547dce39957b890bdaa202c3d0bb4b16
Matheus Iago dos Santos Silva <162377902+iago-developer@users.noreply.github.com>
2024-08-22 16:02:20 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import './App.css';
+import Relogio from './components/Relogio';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 export default function App() {
+    
+    const [num, setNum] = useState(10);
+    let n1 = 10;
 
  return (
   <>
-  <div className="container">
-   <Header />
-   <br/>
-   <Main />
-   <Footer />
-  </div>
+   <p>Valor do state: {num}</p>
+   <button onClick={() => {setNum(num + 100)}}>100</button>
   </>
  )
 }
