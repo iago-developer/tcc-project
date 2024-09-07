7a1c4c317b310e1f796675035841f583dcbb46a2
Matheus Iago dos Santos Silva <162377902+iago-developer@users.noreply.github.com>
2024-09-07 18:36:42 -0300

    Update Medias Queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React,{useState} from "react";
 import './App.css';
 import './medias/Mobile.css';
+import './medias/Laptop.css';
+import './medias/Desktop.css';
+import './medias/Smart.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
