64d5e5bd951950fff268539a03d0fa4148023d9a
Matheus Iago dos Santos Silva <162377902+iago-developer@users.noreply.github.com>
2024-09-26 07:35:46 -0300

    update src/components/Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo_git from "./images/logo_git.webp";
 import Commit from "./images/commit.png";
 import Branch from "./images/branch.png";
